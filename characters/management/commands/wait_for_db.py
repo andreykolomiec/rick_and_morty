8df8488a354bdbf66e2bf48ba6e44e2230ed4b91
Andrew <akolomiec982@gmail.com>
2025-06-11 22:36:48 +0300

    feat: add management command to wait for database before starting app

---
+++ characters/management/commands/wait_for_db.py
@@ -0,0 +1,25 @@
+"""
+реалізуємо кастомну Django-команду, яка чекає, поки база даних буде доступна перед запуском застосунку
+"""
+
+import time
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to pause execution until db is available"""
+
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
+        db_conn = None
+        while not db_conn:
+            try:
+                connections["default"].ensure_connection()
+                db_conn = True
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
