760ef0130ea7c85a06b0d07cc423fb781c4110fc
Andrew <akolomiec982@gmail.com>
2025-06-11 22:50:11 +0300

    refactor: adapt Celery task to support async sync_characters_with_api and add to Docker and make changes to the settings speed up the scraper

--- characters/tasks.py
+++ characters/tasks.py
@@ -6,6 +6,7 @@
 # @shared_task
 # def count_characters() -> int:
 #     return Character.objects.count()
+import asyncio
 
 from characters.scraper import sync_characters_with_api
 
@@ -14,4 +15,4 @@ from celery import shared_task
 
 @shared_task
 def run_sync_with_api() -> None:
-    sync_characters_with_api()
+    asyncio.run(sync_characters_with_api())
